b10d97eb11cc44960be3d35170bd49f35e20fc26
reflevius <reflevius@gmail.com>
2022-12-09 13:11:55 +0300

    feat: implement operations hash compress decompress

--- src/Notifier/Notifier.js
+++ src/Notifier/Notifier.js
@@ -6,6 +6,6 @@ export class Notifier {
 
 	greeting = () => this.logger(`Welcome to the File Manager, ${this.username}!`);
 	farewell = () => this.logger(`\nThank you for using File Manager, ${this.username}, goodbye!`);
-	pwd = (workingDirectory = 'Unknown directory') => this.logger(`You are currently in ${workingDirectory}`);
-	log = (text = 'empty') => this.logger(text);
+	pwd = (workingDirectory) => this.logger(`You are currently in ${workingDirectory ?? 'Unknown directory'}`);
+	log = (text) => this.logger(text ?? 'empty');
 }
